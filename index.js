e5403340e553e44908a21d53376612aa454100c8
Md Khalid Hossain <mdkhalidhossain528@gmail.com>
2026-06-20 00:31:28 +0530

    update server health check message and set default port in index.js; add start script to package.json

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
 import "dotenv/config";
 import express from "express";
 
-const PORT = process.env.PORT
+const PORT = process.env.PORT || 5000
 const app = express()
 
 app.use(express.json())
 
 app.use("/", (req, res)=> {
-    res.json("server is healthy")
+    res.json("server is healthy v1")
 })
 
-app.listen(PORT, () => {
+app.listen(PORT, ()=> {
     console.log(`Server is up and running at http://localhost:${PORT}`);
     
 })
