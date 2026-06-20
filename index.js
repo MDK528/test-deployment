d140861f8b36ae35b19a4f7a5bb0809e509883de
Md Khalid Hossain <mdkhalidhossain528@gmail.com>
2026-06-20 15:39:14 +0530

    update health check message to version 4 in index.js

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ app.use(express.json())
 
 app.use("/", (req, res)=> {
     res.json({
-        message:"server is healthy v3.1", 
+        message:"server is healthy v4", 
         status: "Github-Actions"
     })
 })
